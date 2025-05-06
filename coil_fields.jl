6ea536f182dc5274ac3376df64d3c3dc4fb5f493
Spiffmeister <robospiff@gmail.com>
2025-05-06 16:55:15 +0200

    Updated so that similar results to Florians old code can be replicated, but extended to include a generic psi function instead of only point currents

--- coil_fields.jl
+++ coil_fields.jl
@@ -5,6 +5,13 @@ module coil_fields
 using SpecialFunctions: ellipk, ellipe
 using LinearAlgebra
 using Contour
+using Optim: optimize
+
+
+export Coil, Field, Fourier1D, FourierCurve
+export psi, grad_psi, B_poloidal
+export find_flux_surface
+export derivative, get_RZ, fourier_flux_surface
 
 
 
@@ -23,6 +30,16 @@ struct Coil{TT<:Float64}
     Z   :: TT
     J   :: TT
     Coil(R::TT,Z,J) where TT = new{TT}(R,Z,J)
+end
+
+"""
+Array of coil objects, function ``\\psi`` and the gradient of that function which returns ``(d\\psi/dr, d\\psi/dz)``
+"""
+struct Field{TT<:Float64,PT<:Function,GPT<:Function}
+    Coils       :: Vector{Coil{TT}}
+    ψ           :: PT
+    ∇ψ          :: GPT
+    Field(coilarray::Vector{Coil{TT}},psi,grad_psi) where TT = new{TT,typeof(psi),typeof(grad_psi)}(coilarray,psi,grad_psi)
 end
 
 
@@ -54,7 +71,7 @@ function dGdr(r,z,r′,z′)
 
     k = sqrt(k²)
 
-    dkdr = k / 2r - (r-r′) * k / ((r + r′)^2 + (z - z′)^2)
+    dkdr = k / 2r - (r+r′) * k / ((r + r′)^2 + (z - z′)^2)
 
     dGdr = G(r,z,r′,z′) / 2r + dkdr * sqrt((r + r′)^2 + (z - z′)^2) / (4π*k) * (-2*ellipk(1-tmpvar) + (2-k²)/(1-k²) * ellipe(k²) )
 
@@ -68,7 +85,7 @@ function dGdz(r,z,r′,z′)
 
     dkdz = -(z-z′)*k / ((r + r′)^2 + (z - z′)^2)
 
-    dGdz = dkdz * sqrt((r + r′)^2 + (z - z′)^2)/(4π*k) * (-2*ellipk(1-tmpvar) + (2-k²)/(1-k²) * ellipe(tmpvar))
+    dGdz = dkdz * sqrt((r + r′)^2 + (z - z′)^2)/(4π*k) * (-2*ellipk(1-tmpvar) + (2-k²)/(1-k²) * ellipe(k²))
 
     return dGdz
 end
@@ -77,44 +94,52 @@ end
 
 
 
-
-ψ(coil::Coil,R,Z) = G(R,Z,coil.R,coil.Z) * μ₀ * coil.J
-
-
-function ψ(coils::Array{Coil{TT}}, R, Z) where TT
+"""
+Compute the ``\\psi`` values given a coil or coil array
+"""
+psi(coil::Coil,R,Z) = G(R,Z,coil.R,coil.Z) * μ₀ * coil.J
+function psi(coils::Array{Coil{TT}}, R, Z) where TT
     field_value = TT(0)
-    # @show R, Z
     for coil in coils
-        # @show field_value
-        field_value += ψ(coil,R,Z)
-        # @show field_value
+        field_value += psi(coil,R,Z)
     end
+    return field_value
+end
+function psi(field::Field{TT,PT,GPT},R,Z) where {TT,PT,GPT}
+    field_value = psi(field.Coils,R,Z)
+    field_value += field.ψ(R,Z)
     return field_value
 end
 
 
 
-
+"""
+Derivatives of the ``\\psi`` field
+"""
 dψdr(coil,R,Z) = dGdr(R,Z,coil.R,coil.Z) * μ₀ * coil.J
 
 dψdz(coil,R,Z) = dGdz(R,Z,coil.R,coil.Z) * μ₀ * coil.J
 
+grad_psi(coil,R,Z) = dψdr(coil,R,Z), dψdz(coil,R,Z)
 
-
-∇ψ(coil,R,Z) = dψdr(coil,R,Z), dψdz(coil,R,Z)
-
-function ∇ψ(coils::Array{Coil{TT}},R,Z) where TT
+function grad_psi(coils::Array{Coil{TT}},R,Z) where TT
     dpdr = zero(TT)
     dpdz = zero(TT)
 
     for coil in coils
         dpdr += dψdr(coil,R,Z)
-        dpdz += dψdr(coil,R,Z)
+        dpdz += dψdz(coil,R,Z)
     end
 
     return dpdr, dpdz
 end
-
+function grad_psi(field::Field,R,Z)
+    dpdr, dpdz = grad_psi(field.Coils,R,Z)
+    dpdr_tmp, dpdz_tmp = field.∇ψ(R,Z)
+    dpdr += dpdr_tmp
+    dpdz += dpdz_tmp
+    return dpdz, dpdz
+end
 
 
 
@@ -128,11 +153,26 @@ end
 
 """
 Compute the poloidal magnetic field from a group of coils
-"""
-function B_poloidal(coils::Array{Coil{TT}},R,Z) where TT
-    dpdr, dpdz = ∇ψ(coils,R,Z)
 
+Optional parameter can be the gradient of some ``\\psi`` function which returns the derivatives (dψ/dr,dψ/dz) at a point as a vector or tuple.
+"""
+function B_poloidal(coils::Array{Coil{TT}},R,Z) where {TT}
+    dpdr, dpdz = grad_psi(coils,R,Z)
     return -dpdz/R, dpdr/R
+end
+function B_poloidal(field::Field{TT,PT,GPT},R,Z) where {TT,PT,GPT}
+    B_pr, B_pz = B_poloidal(field.Coils,R,Z)
+    dpdr, dpdz = field.∇ψ(R,Z)
+    B_pr += -dpdz/R
+    B_pz += dpdr/R
+    return B_pr, B_pz
+end
+
+"""
+Compute the toroidal current from a ``\\psi`` field
+"""
+function J_toroidal(field::Field{TT,PT,GPT},R,Z) where {TT,PT,GPT}
+
 end
 
 
@@ -148,64 +188,177 @@ end
 # function find_flux_surface!(R,Z,field,coils;xlims=[-5.0,5.0],ylims=[-5.0,5.0])
 # end
 """
-Finds a flux surface with a given value of ψ
+Finds a flux surface with the ``\\psi`` value at the location ``R,Z``.
 """
-function find_flux_surface(R::TT,Z::TT,gridx,gridy,field::Matrix{TT},coils::Array{Coil{TT}}) where TT
+function find_flux_surface(R::TT,Z::TT,gridx,gridy,field::Matrix{TT},coils_or_field::FT) where {TT,FT<:Union{Array{Coil{TT}},Field}}
 
     local flux_surface
+    old_dist = Inf
+    dist = 0.0
 
-    contour_value = coil_fields.ψ(coils,R,Z)
+    contour_value = psi(coils_or_field,R,Z)
     contour_trace = Contour.contour(gridx,gridy,field,contour_value)
 
+    # minimum_distance = minimum(abs.(line.vertices .- ))
 
-    # We want the periodic contour
+    
     for line in contour_trace.lines
+        # We want the periodic contour
         if line.vertices[1] == line.vertices[end]
-            flux_surface = line
-            continue
+            # Make sure we pick the closest contour to the (R,Z) point given
+            dist = mapreduce(x->norm(x .- (R,Z)), min, line.vertices)
+            if dist < old_dist
+                old_dist = dist
+                flux_surface = line
+            end
         end
     end
 
+    # Store verts in column vector
     flux_surface_verts = hcat([[X[1],X[2]] for X in flux_surface.vertices]...)
 
-    segment_lengths = [norm(flux_surface_verts[:,i+1] - flux_surface_verts[:,i]) for i in Base.OneTo(length(flux_surface.vertices)-1)]
-
-    curve_length = sum(segment_lengths)
-
-    t(i) = segment_lengths[i]/curve_length
-
-
-
-
+    return flux_surface_verts
 end
 
 
-struct Fourier{TT,MODES}
-    R_cosine    ::Vector{TT}
-    R_sine      ::Vector{TT}
-    Z_cosine    ::Vector{TT}
-    Z_sine      ::Vector{TT}
-    Fourier(m) = new{Float64,m}(zeros(Float64,m+1),zeros(Float64,m+1),zeros(Float64,m+1),zeros(Float64,m+1))
+
+struct Fourier1D{TT,MODES}
+    cosine    ::Vector{TT}
+    sine      ::Vector{TT}
+    cosine_modes    :: Vector{Int}
+    sine_modes      :: Vector{Int}
+    Fourier1D(m) = new{Float64,m}(zeros(Float64,m),zeros(Float64,m-1),collect(0:m-1),collect(1:m-1))
 end
 
-function fourier_flux_surface(flux_surface, number_of_modes, symmetric=true)
+struct FourierCurve{TT,MODES}
+    R   :: Fourier1D{TT,MODES}
+    Z   :: Fourier1D{TT,MODES}
+    FourierCurve(m) = new{Float64,m}(Fourier1D(m), Fourier1D(m))
+end
 
-    FSeries = Fourier(number_of_modes)
+_get_all_coefficients(ℱ::Fourier1D) = vcat(ℱ.cosine, ℱ.sine)
+_get_all_coefficients(ℱc::FourierCurve) = vcat(_get_all_coefficients(ℱc.R), _get_all_coefficients(ℱc.Z))
 
+_get_all_modes(ℱ::Fourier1D) = vcat(ℱ.cosine_modes, ℱ.sine_modes)
+_get_all_modes(ℱc::FourierCurve) = vcat(_get_all_modes(ℱc.R), _get_all_modes(ℱc.Z))
+
+_get_nonzero_modes(ℱ::Fourier1D) = vcat(ℱ.cosine .!= 0, ℱ.sine .!= 0)
+
+function _update_coefficients(ℱ::Fourier1D{TT,MODES},coefficients::Array{TT}) where {TT,MODES}
+    ℱ.cosine .= coefficients[1:MODES]
+    ℱ.sine .= coefficients[MODES+1:end]
+end
+function _update_coefficients(ℱc::FourierCurve{TT,MODES},coefficients::Array{TT}) where {TT,MODES}
+    modes = 2MODES-1
+    _update_coefficients(ℱc.R,coefficients[1:modes])
+    _update_coefficients(ℱc.Z,coefficients[modes+1:end])
+end
+# _get_all_modes(ℱ::Fourier1D)
+
+
+Fourier_cosine(ℱ,t) = mapreduce(ai -> ai[2]*cos((ai[1]-1)*t*2π), +, enumerate(ℱ.cosine))
+Fourier_sine(ℱ,t) = mapreduce(ai -> ai[2]*sin(ai[1]*t*2π), +, enumerate(ℱ.sine)) #TODO: need to ensure that sine has less terms (skip the zero term)
+
+(ℱ::Fourier1D)(t) = Fourier_cosine(ℱ,t) + Fourier_sine(ℱ,t)
+(ℱc::FourierCurve)(t) = ℱc.R(t), ℱc.Z(t)
+
+function derivative(ℱ::Fourier1D{TT,MODES},t) where {TT,MODES}
+    dxdt = zero(TT)
+    for I in 2:MODES
+        dxdt += -(I-1) * 2π * ℱ.cosine[I] * sin((I-1) * t * 2π)
+    end
+    for I in 1:MODES-1
+        dxdt += I * 2π * ℱ.sine[I] * cos(I * t * 2π)
+    end
+    return dxdt
+end
+derivative(ℱc::FourierCurve{TT},t) where TT = derivative(ℱc.R,t), derivative(ℱc.Z,t)
+
+
+
+"""
+Get the (R,Z) coordinates of a curve defined by Fourier series
+"""
+function get_RZ(ℱc::FourierCurve{TT,MODES}, N; offset=zero(TT), endpoint=false) where {TT,MODES}
+    endpoint ? N_offset = 1 : N_offset = 0
+    θ = range(zero(TT) + offset, one(TT)+offset, N+1)[1:N+N_offset]
+    R = map(ℱc.R, θ)
+    Z = map(ℱc.Z, θ)
+    return R,Z
+end
+
+
+"""
+Convert a flux surface defined by a set of points to a Fourier series representation
+"""
+function fourier_flux_surface(flux_surface, number_of_modes, coils_or_field::CFT) where {TT,CFT<:Union{Vector{Coil{TT}}, Field}}
+
+    curve = FourierCurve(number_of_modes)
+
+    ψ₀ = psi(coils_or_field, flux_surface[1,1], flux_surface[2,1])
+
+    # Find a bounding box to initialise the flux surface
     xlims = minimum(flux_surface[1,:]), maximum(flux_surface[1,:])
     ylims = minimum(flux_surface[2,:]), maximum(flux_surface[2,:])
     
-    FSeries.R_cosine[1] = +(xlims...) * 0.5
-    FSeries.R_cosine[2] = -(xlims...) * 0.4 #TODO: why do this scaling?
-    FSeries.Z_sine[2] = ylims[2]
+    # Initial guess for the flux surface shape
+    curve.R.cosine[1] = +(xlims...) * 0.5
+    curve.R.cosine[2] = (xlims[2] - xlims[1]) * 0.4 #TODO: why do this scaling?
+    curve.Z.sine[1] = ylims[2] * 0.8
 
-    for mode in Base.OneTo(number_of_modes)
-        FSeries.R_cosine
-        if symmetric
-        end
+    for mode in 2:number_of_modes
+
+        N_I = 1+4mode #Number of integration points
+
+        coefficients = _get_all_coefficients(curve)
+
+        new_coefficients = optimize((coefficients) -> _minimum_psi(coefficients, curve, coils_or_field, ψ₀, N_I), coefficients)
+
+        _update_coefficients(curve, new_coefficients.minimizer)
     end
 
+    return curve
+
 end
+
+
+"""
+Squared normalised distance to the target ``\\psi`` value
+"""
+function distance_to_target(ℱc::FourierCurve, coils_or_field::CFT, target, N_I) where {TT,CFT<:Union{Vector{Coil{TT}}, Field}}
+    R,Z = get_RZ(ℱc, N_I)
+    ψ_curve = [psi(coils_or_field, r, z) for (r,z) in zip(R,Z)]
+    return (ψ_curve .- target).^2 / target.^2
+end
+
+function Bdotn(ℱc::FourierCurve, coils_or_field::CFT, N_I) where {TT,CFT<:Union{Vector{Coil{TT}}, Field}}
+    θ = range(0.0,1.0,N_I+1)[1:N_I]
+    R,Z = get_RZ(ℱc, N_I)
+    B_p = [B_poloidal(coils_or_field,r,z) for (r,z) in zip(R,Z)]
+    B_pr = first.(B_p)
+    B_pz = last.(B_p)
+    tmp_derivative = [derivative(ℱc, ϑ) for ϑ in θ]
+    n_z = -first.(tmp_derivative) # n_z = -dr/dt
+    n_r = last.(tmp_derivative) # n_r = dz/dt
+
+    out = @. (n_r*B_pr + n_z*B_pz)^2 / ((B_pr^2 + B_pz^2)*(n_z^2 + n_r^2))
+    
+    return out
+end
+
+"""
+Function to minimise
+"""
+function _minimum_psi(coefficients,ℱc,coils_or_field,ψ_target,N_I; p=1, q=p)
+
+    _update_coefficients(ℱc, coefficients)
+
+    mscale = 1e-3 * dot(_get_all_modes(ℱc).^(p+q), coefficients.^2) / dot(_get_all_modes(ℱc).^p, coefficients.^2)
+
+    Δψ = sum(distance_to_target(ℱc, coils_or_field, ψ_target, N_I))/2N_I + sum(Bdotn(ℱc, coils_or_field, N_I))/2N_I + mscale
+    return Δψ
+end
+
 
 
 
--- coil_psi_field.jl
+++ coil_psi_field.jl
@@ -1,10 +1,14 @@
 
 
 include("./coil_fields.jl")
+# using Revise
+# using .coil_fields
 
 using GLMakie
 
 using Contour
+
+using Optim
 
 
 
@@ -35,112 +39,161 @@ R, Z , factor,  number, name, Current
 CoilArray = [coil_fields.Coil(R,Z,J) for (R,Z,J) in zip(CoilRs,CoilZs,CoilJs)]
 
 
-
-"""
-
-"""
-
-PlasmaRs = [3.5]
-PlasmaZs = [0.0]
-PlasmaJs = [-4.2e4]
-
-PlasmaArray = [coil_fields.Coil(R,Z,J) for (R,Z,J) in zip(PlasmaRs,PlasmaZs,PlasmaJs)]
-
-
-X = range(0.1,11.0,1001)
-Y = range(-7.0,7.0,1001)
+currentarray = CoilArray
 
 
 
 
-Z_coils = [coil_fields.ψ(CoilArray,x,y) for x in X, y in Y]
+# fsurface = coil_fields.find_flux_surface(3.15, 0.0, X, Y, Z_full, currentarray)
 
-Z_plasma = [coil_fields.ψ(PlasmaArray,x,y) for x in X, y in Y]
+# number_of_modes = 3
+
+# fourier_surface = coil_fields.fourier_flux_surface(fsurface,number_of_modes,currentarray)
 
 
 
-
-ψ_field(r,z) = 5exp(-2(25(r-3)^2 + 5(z-0.0)^2))
+# ψ_field(r,z) = 5exp(-2(25(r-3)^2 + 5(z-0.0)^2))
 
 
 
 
 
 
-# function ψ_field(r,z)
-#     r_0, z_0 = (3.0,0.0)
-#     a_r, a_z = (1.0,1.0)
-#     α = 1
-#     ω = 2
 
-#     R = sqrt( a_r*(r-r_0-2π)^2 + a_z*(z-z_0-2π)^2 )
+function ψ_field(r,z)
+    # Radial ψ field with
 
-#     return α*tanh(ω*R) + 1
-# end
-
-
-
-function f(r,z)
-
-    R = x
-    α = [10.0,10.0]     # Slope strength
-    R_0 = [-3.0, 0.0]   # Origin
-    w = [0.0, 0.0]      # Width
-
-    if R < 0
-        return 1 / (1 + exp(-α * (R - R_0 - w)))
-    else
-        return -1 / (1 + exp(-α * (R + R_0 + w))) + 1
-    end
-
+    peak = 1.0
     
+    α_r, α_z    = 100.0,50.0 # Slope strength
+    R₀, Z₀      = 3.0, 0.0 # Origin
+    w_r         = 5.0 # Width
+
+    R = -sqrt( (r-R₀)^2*α_r + (z-Z₀)^2*α_z) + w_r
+
+    return peak * (tanh(R) + 1)/2
 end
 
 
+function gradient_psi_field(r,z)
+    peak = 1.0
+    α_r, α_z = 100.0, 50.0
+    R₀, Z₀ = 3.0, 0.0
+    w_r = 5.0
+
+    R = -sqrt( (r-R₀)^2*α_r + (z-Z₀)^2*α_z) + w_r
+    
+    dRdr = -α_r*r / R
+    dRdz = -α_z*z / R
+
+    dψdr = peak * dRdr * (1 - tanh(R)^2)
+    dψdz = peak * dRdz * (1 - tanh(R)^2)
+
+    return dψdr, dψdz
+
+end
 
 
-Z_full = 0Z_coils .- [ψ_field(x,y) for x in X, y in Y]
+function bisection(fn, window, tol=1e-2)
+    err = 1.0
+    pt = +(window...)/2
+    while err > tol
+        q = fn(pt)
+        if q*fn(window[1]) < 0.0
+            window[1] = pt
+        else
+            window[2] = pt
+        end
+        pt = +(window...)/2
+        err = abs(q - fn(pt))
+    end
+    return pt
+end
 
 
-# Z_full = Z_coils .+ Z_plasma
+field = coil_fields.Field(currentarray, ψ_field, gradient_psi_field)
 
+@show coil_fields.psi(field, 3.5, 0.0)
+@show coil_fields.B_poloidal(field, 3.15, 0.0)
 
-contour_xy = [3.5,0.0]
-contour_value = coil_fields.ψ(CoilArray,contour_xy...)
-contour_trace = Contour.contour(X,Y,Z_full,contour_value)
-# contour_verts = hcat([[X[1],X[2]] for X in contour_trace.lines[3].vertices]...)
-
-
+@show bisection(R->coil_fields.psi(field,R,0.0),[3.0,4.0])
 
 
 
 
+
+
+X = range(0.1,11,1001)
+Y = range(-7,7,1001)
+
+Z_coils = [coil_fields.psi(currentarray,x,y) for x in X, y in Y]
+
+Z_full = Z_coils .+ [ψ_field(x,y) for x in X, y in Y]
+
+
+# contour_xy = [3.5,0.0]
+# contour_value = coil_fields.psi(field,contour_xy...)
+# contour_trace = Contour.contour(X,Y,Z_full,contour_value)
+
+
+fsurface = coil_fields.find_flux_surface(3.5, 0.0, X, Y, Z_full,field)
+fourier_surface = coil_fields.fourier_flux_surface(fsurface, 2, field)
 
 
 
 f = Figure(); 
 axf = Axis(f[1,1])
 contour!(axf,X,Y,Z_full, levels=500)
-Colorbar(f[1,3],limits=(minimum(Z_full),maximum(Z_full)))
 scatter!(axf,CoilRs,CoilZs,color=:red)
+
+vlines!(axf, [3.0], color=:red, linestyle=:dash)
+hlines!(axf, [0.0], color=:red, linestyle=:dash)
+
+# contour!(axf,X,Y,Z_full, levels=[1e-8], color=:black)
+# xlims!(axf2,1,4); ylims!(axf2,-3,3)
+
+lines!(axf, fsurface[1,:], fsurface[2,:], color=:black)
+
+axf2 = Axis(f[1,2])
+lines!(axf2, ψ_field.(3.0,Y), Y)
+lines!(axf2, Z_full[argmin(abs.(X.-3.0)),:], Y)
+hlines!(axf2, [0.0], color=:red)
+
+axf3 = Axis(f[2,1])
+axf3_cl1 = lines!(axf3, X, ψ_field.(X,0.0), label=L"$\psi$ function")
+axf3_cl2 = lines!(axf3, X, Z_full[:,501], label=L"Full $\psi$")
+vlines!(axf3, [3.0], color=:red)
+
+axislegend(axf3)
+
+Colorbar(f[1,3],limits=(minimum(Z_full),maximum(Z_full)))
+
+
+
 # scatter!(axf,PlasmaRs,PlasmaZs,color=:black)
 
 # scatter!(axf,contour_verts[1,:], contour_verts[2,:],color=:red)
 
-contour!(axf,X,Y,Z_full,levels=[-1e-8],color=:black)
+# contour!(axf,X,Y,Z_full,levels=[coil_fields.psi(currentarray, 3.15,0.0)],color=:black)
 
-xlims!(axf,0.1,10)
-ylims!(axf,-5,5)
+fcurveR,fcurveZ = coil_fields.get_RZ(fourier_surface,40, endpoint=true)
+
+scatterlines!(axf, fcurveR, fcurveZ, color=:red)
+
+
+# xlims!(axf,2,5)
+# ylims!(axf,-2,2)
 
 
 colsize!(f.layout, 1, Relative(0.8))
 rowsize!(f.layout, 1, Relative(0.8))
 
-axf2 = Axis(f[2,1])
-lines!(axf2, X, ψ_field.(X,0.0))
+# axf2 = Axis(f[2,1])
+# lines!(axf2, X, ψ_field.(X,0.0), label=L"\psi")
+# axislegend(axf2)
 
-axf3 = Axis(f[1,2])
-lines!(axf3, Y, ψ_field.(3.0,Y))
+# axf3 = Axis(f[1,2])
+# lines!(axf3, Y, ψ_field.(3.0,Y), label=L"\psi")
 
 
 
